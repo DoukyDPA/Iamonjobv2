79f5c196153fa92d2998026b71ea7bbf92c69149
DoukyDPA <daniel.p.angelini@gmail.com>
2025-09-25 16:39:33 +0200

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,8 +3,8 @@ import os
 
 # Serveur
 bind = f"0.0.0.0:{os.environ.get('PORT', 8080)}"
-workers = 3
-threads = 3
+workers = 1
+threads = 2
 worker_class = "sync"
 worker_connections = 1000
 timeout = 120
@@ -17,7 +17,7 @@ loglevel = "info"
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s'
 
 # Performance
-preload_app = True
+preload_app = False
 max_requests = 1000
 max_requests_jitter = 100
 
