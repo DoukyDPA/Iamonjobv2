dba70c194069caa0013b9b106baeff798bdfc247
DoukyDPA <daniel.p.angelini@gmail.com>
2025-09-25 17:57:40 +0200

    Create GDPRPage.js

---
+++ frontend/src/pages/GDPRPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GDPRUserInterface from '../components/GDPR/GDPRUserInterface';
+import './GDPRPage.css';
+
+const GDPRPage = () => {
+  return (
+    <div className="gdpr-page">
+      <GDPRUserInterface />
+    </div>
+  );
+};
+
+export default GDPRPage;
